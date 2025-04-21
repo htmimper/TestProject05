177f8a32da66de897b30204acc6304a874f1bef5
htmimper <htmimper@gmail.com>
2025-04-21 21:32:50 +0900

    Add files via upload

---
+++ TestProject05/src/test04.java
@@ -0,0 +1,31 @@
+
+public class test04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//Java_15
+		int i = 1;
+		do {
+		System.out.println(i); 
+		i--;
+		}while ( i > 1 );
+		
+		int a = 0;
+		do {
+		System.out.println(a);
+		a++;
+		}while(a < 10 );
+		//aが10未満の時、値が１ずつ増える
+		int b= 5;
+		do {
+		System.out.println(b);
+		b++;
+		} while(b < 10 );
+		int d= 5;
+		do {
+		System.out.println(d);
+		d--;
+		} while(d >0 );
+	}
+
+}
